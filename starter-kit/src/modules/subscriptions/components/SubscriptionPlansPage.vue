b40aeeed8271f2a9e89348c9255921e525528b3e
timz <timugatu@mail.ru>
2025-09-27 20:47:08 +0300

    snapshot

--- starter-kit/src/modules/subscriptions/components/SubscriptionPlansPage.vue
+++ starter-kit/src/modules/subscriptions/components/SubscriptionPlansPage.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import AppPricing from '@modules/subscriptions/components/AppPricing.vue'
+import CenteredContentWrapper from '@core/components/templates/CenteredContentWrapper.vue'
 </script>
 
 <template>
-  <!-- 👉 App Pricing components -->
-  <VRow>
-    <VCol cols="12" sm="8" md="12" lg="10" class="mx-auto">
-      <AppPricing md="4" />
-    </VCol>
-  </VRow>
+  <CenteredContentWrapper>
+    <VRow>
+      <VCol cols="12" sm="8" md="12" lg="10" class="mx-auto">
+        <AppPricing md="4" />
+      </VCol>
+    </VRow>
+  </CenteredContentWrapper>
 </template>
