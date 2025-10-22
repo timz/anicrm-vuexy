3abd69a4c9e17d3d4aefa02264cd63c481a3c94c
timz <timugatu@mail.ru>
2025-10-22 22:24:00 +0300

    snapshot

--- starter-kit/src/modules/subscriptions/components/HistoryPage.vue
+++ starter-kit/src/modules/subscriptions/components/HistoryPage.vue
@@ -39,6 +39,43 @@
             Продлить текущий
           </crud-button-primary>
 
+
+          <VDialog
+            v-model="isRenewDialogVisible"
+            persistent
+            class="v-dialog-sm"
+          >
+            <!-- Dialog Activator -->
+            <template #activator="{ props }">
+              <VBtn v-bind="props">
+                Open Dialog
+              </VBtn>
+            </template>
+
+            <!-- Dialog close btn -->
+            <DialogCloseBtn @click="isRenewDialogVisible = !isRenewDialogVisible" />
+
+            <!-- Dialog Content -->
+            <VCard title="Use Google's location service?">
+              <VCardText>
+                Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
+              </VCardText>
+
+              <VCardText class="d-flex justify-end gap-3 flex-wrap">
+                <VBtn
+                  color="secondary"
+                  variant="tonal"
+                  @click="isDialogVisible = false"
+                >
+                  Disagree
+                </VBtn>
+                <VBtn @click="isDialogVisible = false">
+                  Agree
+                </VBtn>
+              </VCardText>
+            </VCard>
+          </VDialog>
+
           <crud-button-primary size="small"   :to="{ name: 'changeSubscriptionPage' }">
             Изменить тариф
           </crud-button-primary>
@@ -94,6 +131,8 @@ const { t } = useI18n()
 
 // Данные о текущем тарифе
 const activePlanInfo = ref<ActivePlanInfoDto | null>(null)
+
+const isRenewDialogVisible = ref(false)
 
 interface BillingPaymentItem {
   id: string
